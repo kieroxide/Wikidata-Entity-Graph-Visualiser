66f30841005283c2f5715aedb76901e7d8e20a1b
Kieroxide <kieranbprivate:gmail.com>
2025-09-30 14:51:50 +0100

    Added updated backend

--- FDG/backend/py/Cleaner/Cleaner.py
+++ FDG/backend/py/Cleaner/Cleaner.py
@@ -1,98 +1,33 @@
-from .CleanerUtility import remove_unconnected_vertices, filter_invalid_relations
+from .CleanerUtility import (
+    ensure_one_component, filter_invalid_relations, 
+    find_no_label_entities, find_no_label_properties, 
+    find_unreferenced_entities, remove_unconnected_vertices
+)
 
 class Cleaner:
     @staticmethod
-    def clean_data(entities: dict, properties: dict, relations: dict, console=False):
-        """Combs through data culling any incombatible data, Currently properties are not touched
-        as I have come across no errors"""
-
-        # Removes entities and properties with no labels/ bad labels
-        # Removes relations that reference missing entities/properties
-        # Remove entities with no relations
-
-        no_label_entities = Cleaner.find_no_label_entities(entities, console)
-        
+    def clean_data(source_id: str, entities: dict, properties: dict, relations: dict, console=False):
+        """
+        Clean entities, properties, and relations by removing items with missing or invalid labels,
+        and entities with no relations. Properties are not heavily filtered.
+        """
         # Remove entities with no labels
+        no_label_entities = find_no_label_entities(entities, console)
         for no_label_entity in no_label_entities:
             entities.pop(no_label_entity, None)
 
-        # We dont remove edges with no-label as too restrictive
-        no_label_properties = Cleaner.find_no_label_properties(properties, console)
+        no_label_properties = find_no_label_properties(properties, console)
+        for prop_id in no_label_properties:
+            properties.pop(prop_id)
         
-        unreferenced_entity_ids = Cleaner.find_unreferenced_entities(entities, relations, console)
-
-        #no need to cull relations as this is done in frontend
-        #relations = filter_invalid_relations(relations, entities, properties)
+        relations = filter_invalid_relations(relations, entities, properties)
+        
+        unreferenced_entity_ids = find_unreferenced_entities(entities, relations, console)
+        for unreference_entity_id in unreferenced_entity_ids:
+            entities.pop(unreference_entity_id, None)
 
         entities = remove_unconnected_vertices(entities, relations)
 
+        entities, relations = ensure_one_component(source_id ,relations, entities, )
+
         return entities, properties, relations
-    
-    @staticmethod
-    def find_no_label_entities(entities, console=False):
-        if not entities:
-            return set()
-        
-        no_label_ids = set()
-        
-        for entity_id, entity_data in entities.items():
-            entity_label = entity_data.get("label", entity_id)
-            
-            # Tests for unknown label entities
-            if entity_id == entity_label:
-                no_label_ids.add(entity_id)
-
-        if no_label_ids:
-            if console:
-                print(f"Label Test Failed: {no_label_ids}")
-            return no_label_ids
-        elif console:
-            print("Entity test passed!")
-
-        return set()
-    
-    @staticmethod
-    def find_no_label_properties(properties, console=False):
-        if not properties:
-            return set()
-        
-        no_label_ids = set()
-        
-        for prop_id, prop_label in properties.items():
-            # Tests for unknown label entities
-            if prop_id == prop_label:
-                no_label_ids.add(prop_id)
-        
-        if no_label_ids:
-            if console:
-                print(f"Label Test Failed: {no_label_ids}")
-            return no_label_ids
-
-        if console:
-            print("Entity test passed!")
-        return set()
-        
-    def find_unreferenced_entities(entities, relations, console=False):
-        all_ids = set()
-
-        for source_id, relation in relations.items():
-            all_ids.add(source_id)
-            for _, target_ids in relation.items():
-                for target_id in target_ids:
-                     all_ids.add(target_id)
-
-        non_mapped_ids = set()
-        for id in all_ids:
-            if id not in entities.keys():
-                non_mapped_ids.add(id)
-
-        if non_mapped_ids:
-            if console:
-                print(non_mapped_ids)
-            return non_mapped_ids
-        else:
-            if console:
-                print("All relation test passed!")
-            return set()
-
-
