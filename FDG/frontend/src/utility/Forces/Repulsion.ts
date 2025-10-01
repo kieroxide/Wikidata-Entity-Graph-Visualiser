355a21162da5c39c4d7f38aa05aec2df2308f24b
Kieroxide <kieranbprivate:gmail.com>
2025-10-01 21:58:19 +0100

    Fixed Repulsion accidentally being too large

--- FDG/frontend/src/utility/Forces/Repulsion.ts
+++ FDG/frontend/src/utility/Forces/Repulsion.ts
@@ -6,7 +6,7 @@ import { repulsion } from "../Forces/Rust/fdg_wasm/pkg/fdg_wasm";
  * Static class for applying repulsion forces to vertices.
  */
 export class Repulsion {
-    private static readonly STRENGTH = 8000;
+    private static readonly STRENGTH = 4000;
     private static readonly DISTANCE_CUTOFF = 4000;
 
     /** Applies repulsive forces between all pairs of vertices using the WASM-accelerated function. */
